680c985773e19867a24f9032edd3ff5a5cf1f561
Gustavo Braga <gh.brga@gmail.com>
2021-09-15 23:06:41 -0300

    Atividade avaliativa ADO 2 concluida

---
+++ Modal com Flatlist/Ado02/modal-com-get-e-header/App.js
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, FlatList, Pressable, Image,Modal } from 'react-native';
+import Constants from 'expo-constants';
+
+async function executeGet(url,jsonState){
+    //get síncrono com o uso do fetch
+    await fetch(url)
+    .then(response => {
+          if (response.status === 200) {
+            console.log('sucesso');
+            response.json().then(function(result){ 
+
+              //console.log(result);
+              jsonState(result)
+
+              });
+          } else {
+            throw new Error('Erro ao consumir a API!');
+          }
+      })
+      .then(response => {
+        //console.debug(response);
+      }).catch(error => {
+        console.error(error);
+      });
+  }
+
+
+
+const ShowDetalhes = ({display,toogleModal,mensagem}) => (   
+    <Modal
+          animationType="slide"
+          transparent={true}
+          visible={display}
+          onRequestClose={toogleModal}
+    >
+
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+                <Pressable onPress={toogleModal}>
+                  <Text>{mensagem}</Text>
+                </Pressable>
+          </View>
+        </View>
+    
+    </Modal>
+        
+ )
+
+const Pessoa = ({name,email}) => {
+    
+    //state para controle do Modal
+    const [modal,setModal] = React.useState(false)
+
+    function mudaModal(){
+      setModal(!modal)
+    }
+
+    return(
+    <View>
+      <ShowDetalhes display={modal} toogleModal={mudaModal} mensagem={email}/>
+      
+      <Pressable onPress={mudaModal}>
+        <Text style={styles.paragraph}>{name}</Text>
+      </Pressable>
+    </View>
+    )
+}
+
+
+export default function App() {
+
+  const [jsonData,setJsonData] = React.useState({})
+
+  executeGet("https://jsonplaceholder.typicode.com/users",setJsonData)
+
+  //função que renderiza cada item do FlatList
+  function meuItem({item}){
+    let nomeCompleto = item.name
+    
+    return(
+      <Pessoa name={item.name}
+              email={item.email} 
+      />
+    )
+  }
+  function headerList (){
+    return (
+      <Text style={styles.header}>Lista de usuarios</Text>
+    )
+  }
+    
+  
+  return (
+
+    <View style={styles.container}>
+
+        <FlatList
+          data={jsonData}
+          renderItem={meuItem}
+          keyExtractor={item => item.id}
+          ListHeaderComponent={headerList}
+        />     
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 12,
+    padding: 12,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'pink'
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    alignSelf: 'center'
+  },
+  header: {
+    margin: 0,
+    padding: 12,
+    fontSize: 18,
+    width: "100%",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color:'white',
+    backgroundColor: 'black'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
